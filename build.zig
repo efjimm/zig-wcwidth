91891b022dd680dfac8a8ce703418cac65db8ee6
evan <evanbonner99@gmail.com>
2023-04-26 21:18:56 +0100

    Initial commit

---
+++ build.zig
@@ -0,0 +1,14 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+	_ = b.addModule("wcwidth", .{
+		.source_file = .{ .path = "src/wcwidth.zig" },
+	});
+
+	const tests = b.addTest(.{
+		.root_source_file = .{ .path = "src/wcwidth.zig" },
+	});
+	const run_tests = b.addRunArtifact(tests);
+	const test_step = b.step("test", "Run all tests");
+	test_step.dependOn(&run_tests.step);
+}
