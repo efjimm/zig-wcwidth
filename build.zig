74e2c69a96ce3d86d8340668cdefe5999ab2eef0
evan <evanbonner99@gmail.com>
2024-01-09 04:59:24 +0000

    Allow passing target and optimize modes

--- build.zig
+++ build.zig
@@ -1,12 +1,19 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
     _ = b.addModule("wcwidth", .{
         .root_source_file = .{ .path = "src/wcwidth.zig" },
+        .target = target,
+        .optimize = optimize,
     });
 
     const tests = b.addTest(.{
         .root_source_file = .{ .path = "src/wcwidth.zig" },
+        .target = target,
+        .optimize = optimize,
     });
     const run_tests = b.addRunArtifact(tests);
     const test_step = b.step("test", "Run all tests");
